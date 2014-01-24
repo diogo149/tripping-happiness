3eb62a64e53f4dc7a836180f0b747690aa075025
Diogo Moitinho de Almeida <diogo149@gmail.com>
2014-01-24 14:27:49 -0800

    updating users recipe

--- users/recipes/default.rb
+++ users/recipes/default.rb
@@ -1,10 +1,13 @@
-prod = node[:prod]
+prod = node[:prod] != nil ? node[:prod] : {}
 prod_user = prod[:user] != nil ? prod[:user] : "prod"
+prod_group = prod[:group] != nil ? prod[:group] :  prod_user
+
+group prod_group
 
 user prod_user do
   supports :manage_home => true
   home "/home/#{prod_user}"
   shell prod[:shell] != nil ? prod[:shell] : "/bin/bash"
-  group prod[:group] != nil ? prod[:group] :  prod_user
+  group prod_group
   action :create
 end
